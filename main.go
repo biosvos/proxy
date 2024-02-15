096418b7fde3ca2443b6dbe24ddb1fb8f6e2a8a4
jinsu <biosvos@gmail.com>
2024-02-16 07:21:03 +0900

    base

--- main.go
+++ main.go
@@ -1,9 +1,67 @@
 package main
 
 import (
+	"io"
 	"log"
+	"net/http"
+	"time"
 )
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	targetURL := r.URL
+	proxyReq, err := http.NewRequestWithContext(r.Context(), r.Method, targetURL.String(), r.Body)
+	if err != nil {
+		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
+		return
+	}
+
+	for name, values := range r.Header {
+		for _, value := range values {
+			proxyReq.Header.Add(name, value)
+		}
+	}
+
+	tp := http.DefaultTransport
+
+	resp, err := tp.RoundTrip(proxyReq)
+	if err != nil {
+		http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		log.Printf("failed to copy: %v", err)
+	}
+}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	const idleTimeout = 30 * time.Second
+	const writeTimeout = 1 * time.Second
+	const readTimeout = 1 * time.Second
+	const readHeaderTimeout = 2 * time.Second
+	server := http.Server{
+		ReadTimeout:       readTimeout,
+		WriteTimeout:      writeTimeout,
+		IdleTimeout:       idleTimeout,
+		ReadHeaderTimeout: readHeaderTimeout,
+
+		Addr:    ":8787",
+		Handler: http.HandlerFunc(handleRequest),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
